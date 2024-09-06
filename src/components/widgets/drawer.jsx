75e33077ac98f49486facc1b6d9d3b331e8d335d
Vaidik-CRM <vaidik@crmemperor.com>
2024-09-06 11:48:20 +0530

    budget header issue fixed

--- src/components/widgets/drawer.jsx
+++ src/components/widgets/drawer.jsx
@@ -146,14 +146,14 @@ const Budget = (props) => {
               <Icon as={FaUniversalAccess} />
               <Text fontSize={'md'}>Period</Text>
             </Flex>
-            <Flex gap={2} direction={{ base: 'column', sm: 'row' }}>
-              <Flex flex={1} minWidth={{sm: '150px', md: '250px'}}>
+            <Flex gap={2} direction={{ base: 'column', sm: 'row' }} justifyContent={'space-around'} alignItems={'center'}> 
+              <Flex flex={1} width={'100%'} minWidth={{sm: '200px', md: '250px'}} maxWidth={{base:'280px',sm:'200px',md:'300px'}}>
                 <LocationDropDown
                   locationValue={props.location}
                   setLocation={props.handleLocationChange} 
                 />
               </Flex>
-              <Flex flex={1} fontSize={'sm'} width={'100%'} justifyContent={'space-around'}>
+              <Flex flex={1} fontSize={'sm'} width={'100%'} justifyContent={'space-around'} maxWidth={{base:'280px',sm:'200px',md:'300px'}}>
                 <CustomDateRangePicker
                   value={props.dateValue} 
                   dateValue={props.handleDateChange} 
