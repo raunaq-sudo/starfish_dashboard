bc8f71da13259a1f41634794b1f7c1d1b63d0261
raunaq-sudo <rsiraswar@gmail.com>
2024-10-11 00:26:17 +0530

    Ai aiSummary first draft

--- src/components/widgets/aiSummary/aiMonthSummary.jsx
+++ src/components/widgets/aiSummary/aiMonthSummary.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Table, Button, Dropdown, Header } from 'rsuite';
 import apiEndpoint from '../../config/data';
-import { Card, CardBody, CardHeader, Flex, Select, Box } from '@chakra-ui/react';
+import { Card, CardBody, CardHeader, Flex, Select, Box, filter, Modal, ModalCloseButton, ModalHeader, ModalContent, ModalBody, ModalFooter, ModalOverlay, Textarea, Text } from '@chakra-ui/react';
 
 const { Column, HeaderCell, Cell } = Table;
 
@@ -31,10 +31,10 @@ const ActionCell = ({ rowData, dataKey, onClick, ...props }) => {
       <Button
         appearance="link"
         onClick={() => {
-          onClick(rowData.id);
+          onClick(rowData);
         }}
       >
-        {rowData.status === 'EDIT' ? 'Save' : 'Edit'}
+        {'View Output'}
       </Button>
     </Cell>
   );
@@ -53,12 +53,29 @@ class AIMonthSummary extends Component {
         data: [],
         filteredData: [], // This will be filled after filtering
         loading: false,
+        modalOpen:false
       };
       
 
   handleDropdownChange = (value, key) => {
-    this.setState({ [key]: value }, this.filterData);
+    this.setState({ [key]: value});
+    if(key ==='selectedCompany'){
+        this.setState({integration_data:this.generalFilter(value, this.state.company_integration, 'company_id')})
+    }
+    if(key === 'selectedIntegration'){
+      this.setState({location_data:this.generalFilter(value, this.state.integration_location, 'integration_id')})
+    }
   };
+
+
+  generalFilter = (value, array, key) =>{
+    const ddFiltered = array.filter((item) => {
+      return (
+        (item[key] === value)
+      );
+    });
+    return ddFiltered
+  }
 
   handleChange = (id, key, value) => {
     const nextData = Object.assign([], this.state.data);
@@ -98,31 +115,73 @@ class AIMonthSummary extends Component {
   };
 
 
-//   fetchData = async () => {
-//     this.setState({ loading: true });
-//     await fetch(apiEndpoint + '/api/fetch_data/', {
-//       headers: { Authorization: 'Bearer ' + localStorage['access'] },
-//       method: 'GET',
-//     })
-//       .then((response) => response.json())
-//       .then((data) => {
-//         console.log(data);
-//         this.setState({ data, filteredData: data }); // Initially, filteredData is the same as data
-//       })
-//       .catch((err) => console.error(err));
-//     this.setState({ loading: false });
-//   };
-fetchData = () => {
-    // Simulating fetch delay
+  fetchData = async (type) => {
     this.setState({ loading: true });
-    setTimeout(() => {
-      this.setState({
-        filteredData: this.state.data, // Initially set filteredData as the full data
-        loading: false,
-      });
-    }, 1000); // Simulated delay
+    if(type==='aisummary'){
+      await fetch(apiEndpoint + '/api/fetch_data_aisummary/?' + new URLSearchParams({
+        company_id: this.state.selectedCompany,
+        integration_id: this.state.selectedIntegration,
+        location_id: this.state.selectedLocation,
+        year: this.state.selectedYear
+      }), {
+        headers: { Authorization: 'Bearer ' + localStorage['access'] },
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.setState({ data }); 
+        })
+        .catch((err) => console.error(err));
+    }else{
+      await fetch(apiEndpoint + '/api/fetch_data_aisummary/?' + new URLSearchParams({
+        initial_load:'1'
+      }), {
+        headers: { Authorization: 'Bearer ' + localStorage['access'] },
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.setState({ 
+            company_data: data['company'], 
+            company_integration: data['company_integration'], 
+            integration_location: data['integration_location'] 
+          }); 
+        })
+        .catch((err) => console.error(err));
+    }
+    
+    this.setState({ loading: false });
   };
-  
+
+
+// fetchData = () => {
+//     // Simulating fetch delay
+//     this.setState({ loading: true });
+//     setTimeout(() => {
+//       this.setState({
+//         filteredData: this.state.data, // Initially set filteredData as the full data
+//         loading: false,
+//       });
+//     }, 1000); // Simulated delay
+//   };
+  openModal = (rowData)=>{
+    console.log(Object.keys(rowData))
+    this.setState({outputs:[]})
+    const outputs = []
+    Object.keys(rowData).forEach((item)=>{
+      if (item.startsWith('output_')){
+        if (rowData[item]){
+          outputs.push({'output':rowData[item]})
+        }
+      }
+    })
+    this.setState({outputs}, ()=>{
+      this.setState({modalOpen:true})
+    })
+     
+  }
 
   filterData = () => {
     const { data, selectedCompany, selectedIntegration, selectedLocation, selectedYear } = this.state;
@@ -138,14 +197,16 @@ fetchData = () => {
   };
 
   componentDidMount() {
-    this.fetchData();
+    this.fetchData("asd");
   }
 
   render() {
     const { companyOptions, integrationOptions, locationOptions, yearOptions, filteredData, loading } = this.state;
 
     return (
-      <Card minH={700}>
+      <>
+    
+    <Card minH={700}>
        <CardHeader>
         <Flex direction="column" gap={4}>
             {/* First Row with Company and Integration Dropdowns */}
@@ -154,14 +215,14 @@ fetchData = () => {
             <Box flex="1 1 200px" minW="200px">
                 <Header>Company</Header>
                 <Dropdown title={this.state.selectedCompany || 'Select Company'}>
-                {companyOptions.map((company, index) => (
+                {this.state.company_data!==undefined?this.state.company_data.map((item) => (
                     <Dropdown.Item
-                    key={index}
-                    onClick={() => this.handleDropdownChange(company, 'selectedCompany')}
+                    key={item.company_id}
+                    onClick={() => this.handleDropdownChange(item.company_id, 'selectedCompany')}
                     >
-                    {company}
+                    {item.company_name}
                     </Dropdown.Item>
-                ))}
+                )):<></>}
                 </Dropdown>
             </Box>
 
@@ -169,14 +230,14 @@ fetchData = () => {
             <Box flex="1 1 200px" minW="200px">
                 <Header>Integration</Header>
                 <Dropdown title={this.state.selectedIntegration || 'Select Integration'}>
-                {integrationOptions.map((integration, index) => (
+                {this.state.integration_data!==undefined?this.state.integration_data.map((item) => (
                     <Dropdown.Item
-                    key={index}
-                    onClick={() => this.handleDropdownChange(integration, 'selectedIntegration')}
+                    key={item.integration_id}
+                    onClick={() => this.handleDropdownChange(item.integration_id, 'selectedIntegration')}
                     >
-                    {integration}
+                    {item.integration_name}
                     </Dropdown.Item>
-                ))}
+                )):<></>}
                 </Dropdown>
             </Box>
             </Flex>
@@ -202,23 +263,35 @@ fetchData = () => {
             <Box flex="1 1 200px" minW="200px">
                 <Header>Location</Header>
                 <Dropdown title={this.state.selectedLocation || 'Select Location'}>
-                {locationOptions.map((location, index) => (
+                {this.state.location_data!==undefined?this.state.location_data.map((item) => (
                     <Dropdown.Item
-                    key={index}
-                    onClick={() => this.handleDropdownChange(location, 'selectedLocation')}
+                    key={item.location_id}
+                    onClick={() => this.handleDropdownChange(item.location_id, 'selectedLocation')}
                     >
-                    {location}
+                    {item.location_name}
                     </Dropdown.Item>
-                ))}
+                )):<></>}
                 </Dropdown>
             </Box>
             </Flex>
+            
+            {/* submit button */}
+            <Flex gap={4} flexWrap="wrap">
+            {/* Year Dropdown */}
+
+                {/* <Header>Year</Header> */}
+                <Button color='primary' onClick={()=>this.fetchData('aisummary')}>Fetch</Button>
+         
+            </Flex>
+            
+
         </Flex> 
        </CardHeader>
 
 
         <CardBody>
-          <Table height={400} data={filteredData} virtualized rowKey={'id'} loading={loading}>
+       
+          <Table height={400} data={this.state.data} virtualized rowKey={'id'} loading={loading}>
             <Column width={200} align="center" fixed>
               <HeaderCell>Subject</HeaderCell>
               <Cell dataKey="subject" />
@@ -226,31 +299,50 @@ fetchData = () => {
 
             <Column width={150} align="center" fixed>
               <HeaderCell>Created On</HeaderCell>
-              <Cell dataKey="createdOn" />
+              <Cell dataKey="created_date"/>
             </Column>
 
-            <Column width={150} align="center">
+            <Column width={150} align="center"> 
               <HeaderCell>Incorrect</HeaderCell>
-              <EditableCell dataKey="incorrect" onChange={this.handleChange} />
+              <Cell dataKey="incorrect_summary" />
             </Column>
 
             <Column width={100} align="center">
-              <HeaderCell>View</HeaderCell>
-              <Cell dataKey="view" />
+              <HeaderCell>View output</HeaderCell>
+              <ActionCell dataKey="id" onClick={this.openModal}/>
             </Column>
 
-            <Column width={100} align="center">
-              <HeaderCell>PDF</HeaderCell>
-              <Cell dataKey="pdf" />
-            </Column>
-
-            <Column flexGrow={1}>
-              <HeaderCell>Actions</HeaderCell>
-              <ActionCell dataKey="id" onClick={this.handleEditState} />
-            </Column>
+            
           </Table>
+
+          <Modal size={'full'} onClose={()=>this.setState({modalOpen:!this.state.modalOpen})} isOpen={this.state.modalOpen} isCentered>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>AI Summary Output</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Flex wrap={'wrap'}>
+                    {this.state.outputs!==undefined?this.state.outputs.map((item)=>{
+                      <Textarea
+                      placeholder={item['output']}
+                      // onChange={handleInputChange}
+                    />
+                    console.log(item.output)
+                    }):<></>}
+                </Flex>
+            
+
+
+              </ModalBody>
+              <ModalFooter>
+                <Button onClick={()=>this.setState({modalOpen:!this.state.modalOpen})}>Close</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </CardBody>
       </Card>
+
+      </>
     );
   }
 }
